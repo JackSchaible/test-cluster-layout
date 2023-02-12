c5bf735fadbb67229398e4748405edaa9515d79b
Jack Schaible <jack.schaible@hotmail.com>
2023-02-12 13:18:45 -0700

    non-cluster nodes now spawn outwards, cluster nodes spawn anywhere

--- Assets/LayoutEngine.cs
+++ Assets/LayoutEngine.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Vectrosity;
 using Vector2 = UnityEngine.Vector2;
@@ -8,26 +9,22 @@ public class LayoutEngine : MonoBehaviour
 {
     public ResearchNode Root;
     public float BranchLength = 1.0f;
+    public float ClusterBranchLengthModifier = 2.0f;
     public float BranchWidth = 2.0f;
     public float BerrySize = 30.0f;
-    private const float Pi = Mathf.PI;
+    public float NonStraightBranchFactor = 0.4f;
     public GameObject TreeNodePrefab;
     public Canvas Canvas;
 
-    
+    private const float Pi = Mathf.PI;
 
-    // Start is called before the first frame update
-    public void Generate()
-    {
+    public void Generate() =>
         Layout(Root, Vector2.zero);
-    }
 
     public void Clean()
     {
         foreach (Transform child in Canvas.transform)
-        {
             DestroyImmediate(child.gameObject);
-        }
     }
 
     private void Layout(ResearchNode node, Vector2 center, Node parentNode = null)
@@ -46,55 +43,85 @@ public class LayoutEngine : MonoBehaviour
         rectTransform.sizeDelta = new Vector2(2 * treeNode.Radius, 2 * treeNode.Radius);
 
         ResearchNode[] children = node.Children;
-        int n = children.Length;
-        
-        // Updated this to restrict the angle children spawn in to 90 degrees
-        float angleStep = Pi / (2 * n);
-        float childAngle = 0;
+        int nChildren = children.Length;
 
+        float childAngle = 0;
         if (parentNode != null)
         {
             childAngle = Mathf.Atan2(center.y - parentNode.Position.y, center.x - parentNode.Position.x);
             RectTransform parentTransform = parentNode.GetComponent<RectTransform>();
 
-            // Moved these to their own lines to make it easier to read
-            float lineLength = (treeNode.Cluster == parentNode.Cluster ? BranchLength : BranchLength * 2)
+            float lineLength = (treeNode.Cluster == parentNode.Cluster ? BranchLength : BranchLength * ClusterBranchLengthModifier)
                                - BerrySize;
 
             Vector2 lineStart = treeNode.Radius * new Vector2(Mathf.Cos(childAngle), Mathf.Sin(childAngle));
             Vector2 lineEnd = new Vector2(Mathf.Cos(childAngle), Mathf.Sin(childAngle)) * lineLength;
-            
+
             DrawLine(lineStart, lineEnd, parentTransform.anchoredPosition,
                 Canvas, treeNode.Name, parentNode.Name);
-        } else {
-            // Makes the root node spawn children in all directions
-            angleStep = 2 * Pi / n;
         }
-        
+
+        ResearchNode[] nonClusterChildren = children.Where(c => c.Cluster != node.Cluster).ToArray();
+        int nNonClusterChildren = nonClusterChildren.Length;
+        if (nNonClusterChildren > 0 && parentNode != null)
+            LayoutNonClusterChildren(nNonClusterChildren, childAngle, nonClusterChildren, node, center, treeNode);
+        else if (nNonClusterChildren > 0)
+            LayoutClusterChildren(nNonClusterChildren, childAngle, nonClusterChildren, node, center, treeNode, 0);
+
+        ResearchNode[] clusterChildren = children.Where(c => c.Cluster == node.Cluster).ToArray();
+        int nClusterChildren = clusterChildren.Length;
+        if (nClusterChildren > 0)
+            LayoutClusterChildren(nClusterChildren, childAngle, clusterChildren, node, center, treeNode, nNonClusterChildren);
+    }
+
+    private void LayoutNonClusterChildren(int nChildren, float childAngle, ResearchNode[] children,
+        ResearchNode node, Vector3 center, Node treeNode)
+    {
         // Starting point for children angles. Allows us to "walk" out from starting point with every increment of i
-        // 
-        float childStartAngle = childAngle - (angleStep * (Mathf.Ceil(n / 2)));
+        // Updated this to restrict the angle children spawn in to 90 degrees
+        float angleStep = Pi / (2 * nChildren);
+        float childStartAngle = childAngle - angleStep * Mathf.Ceil(nChildren / 2) +
+                                // Make it so we don't always spawn perfectly straight out
+                                (NonStraightBranchFactor * (nChildren % 2 == 0 ? 1 : -1));
+
+        DrawChildren(nChildren, children, node, center, treeNode, childStartAngle, angleStep);
+    }
+
+    private void LayoutClusterChildren(int nChildren, float childAngle, ResearchNode[] children,
+        ResearchNode node, Vector3 center, Node treeNode, int nNonClusterChildren)
+    {
+        // Starting point for children angles. Allows us to "walk" out from starting point with every increment of i
+        // Updated this to restrict the angle children spawn in to 90 degrees
+        float angleStep = 2 * Pi / (nChildren);
+        float childStartAngle = childAngle - angleStep * nChildren + (nNonClusterChildren * angleStep / 2) + 2f;
+
+        DrawChildren(nChildren, children, node, center, treeNode, childStartAngle, angleStep);
+    }
+
+    private void DrawChildren(int nChildren, ResearchNode[] children, ResearchNode node, Vector3 center,
+        Node treeNode, float childStartAngle, float angleStep)
+    {
 
         // Draw children
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < nChildren; i++)
         {
             ResearchNode child = children[i];
             float adjustedRadius = BranchLength;
             if (child.Cluster != node.Cluster)
-                adjustedRadius *= 2;
+                adjustedRadius *= ClusterBranchLengthModifier;
 
             float angle = i * angleStep + childStartAngle;
             float childX = center.x + adjustedRadius * Mathf.Cos(angle);
             float childY = center.y + adjustedRadius * Mathf.Sin(angle);
-            Vector2 childCenter = new (childX, childY);
+            Vector2 childCenter = new(childX, childY);
 
             Layout(child, childCenter, treeNode);
         }
     }
 
-    private void DrawLine(Vector2 start, Vector2 end,Vector2 parentPos, Canvas canvas, string toObjectName, string fromObjectName)
+    private void DrawLine(Vector2 start, Vector2 end, Vector2 parentPos, Canvas canvas, string toObjectName, string fromObjectName)
     {
-        VectorLine line = new ($"Line from {fromObjectName} to {toObjectName}",
+        VectorLine line = new($"Line from {fromObjectName} to {toObjectName}",
             new List<Vector2>(), BranchWidth);
         line.SetCanvas(canvas);
         line.rectTransform.localScale = Vector3.one;
